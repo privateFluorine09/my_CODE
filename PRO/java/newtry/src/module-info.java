e631f865f7a941d5b61a13511dd127f0bdd1359f
privateFluorine09 <wzm0722@163.com>
2018-10-18 11:29:06 +0800

    new java

---
+++ PRO/java/newtry/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author wzm07
+ *
+ */
+module newtry {
+}
---
+++ PRO/java/newtry2/src/helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld
+{
+	public static void main(String args[])
+	{
+		boolean p=true, q=false;
+		
+		System.out.println("Hello World");
+		System.out.println(p&&q);
+		System.out.println(p||q);
+	}
+}
