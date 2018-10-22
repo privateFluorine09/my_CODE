4b0463cb3fa64404f836fdbb8d2c3441b2adc2fe
privateFluorine09 <wzm0722@163.com>
2018-10-22 21:36:32 +0800

    PG.saving()

--- PRO/C++/playground/build as will.cpp
+++ PRO/C++/playground/build as will.cpp
@@ -19,15 +19,52 @@ class UpQueue
    friend std::ostream &operator<< <T> (std::ostream&, const UpQueue&);
 
 public:
+   inline bool empty()const
+   {
+      return (the_size==0)||(the_front==the_rear);
+   }
+
+   inline size_t size()const
+   {
+      return difference(the_rear, the_front);
+   }
+
+   void push(const T&);
+   void pop();
+
+   inline const T &front()const
+   {
+      return que_ptr[the_front];
+   }
+
+   inline const T &back()const
+   {
+      return que_ptr[step_back(the_rear)];
+   }
 
 private:
    size_t the_front=0;
    size_t the_rear=0;
-   size_t the_size=10;
-   unique_ptr<T[]> que_ptr;
+   size_t the_size=0;
+   std::unique_ptr<T[]> que_ptr;
 
-   inline step_forward(const size_t &location, const size_t &steps=1)
+   inline size_t step_forward(const size_t &location, const size_t &steps=1)const
    {
       return (location+steps)%the_size;
    }
+
+   inline size_t step_back(const size_t &location, const size_t &steps=1)const
+   {
+      return (location-steps)%the_size;
+   }
+
+   inline size_t difference(const size_t &location1, const size_t &location2)const //location1-location2
+   {
+      return (the_size+location1-location2)%the_size;
+   }
 };
+
+int main()
+{
+   return 0;
+}
